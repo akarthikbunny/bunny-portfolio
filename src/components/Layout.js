7b99a3fba81ad166777a6814e2973bd13daf1c5d
Karthik Arige <arigekarthik18@gmail.com>
2026-01-10 14:44:41 +0530

    Remove unused siteData prop from Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Layout = ({ children, className = "", siteData }) => {
+const Layout = ({ children, className = "" }) => {
     return (
         <div className={`w-full h-full block z-0 bg-light p-4 dark:bg-dark xl:p-24 lg:p-16 md:p-12 ${className}`}>
             {children}
